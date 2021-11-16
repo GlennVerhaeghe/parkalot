239dda90bf4794118cc4a3d5c21497f6678082ed
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-11-16 10:58:36 +0100

    Initial commit

---
+++ src/main/java/be/parkalot/knight_parkalot/KnightParkalotApplication.java
@@ -0,0 +1,13 @@
+package be.parkalot.knight_parkalot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightParkalotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnightParkalotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/parkalot/knight_parkalot/KnightParkalotApplicationTests.java
@@ -0,0 +1,13 @@
+package be.parkalot.knight_parkalot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnightParkalotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
