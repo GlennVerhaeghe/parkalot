3989508b0c787244b4389abb7d74374987a08e28
ibrahimbow <smartman402@hotmail.com>
2021-11-16 15:54:30 +0100

    data modeling

---
+++ src/main/java/be/parkalot/knight_parkalot/domain/PostalCode.java
@@ -0,0 +1,29 @@
+package be.parkalot.knight_parkalot.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "postalcode")
+public class PostalCode {
+    @Id
+    @Column(name = "code")
+    private int code;
+
+    @Column(name = "city")
+    private String city;
+
+    public PostalCode() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
