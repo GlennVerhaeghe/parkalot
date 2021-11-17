2b25ab4db0b39034af4c53dd63c239a571db2486
ibrahimbow <smartman402@hotmail.com>
2021-11-17 15:37:27 +0100

    validation of creating division

--- src/main/java/be/parkalot/knight_parkalot/service/inputvalidation/DivisionInputValidation.java
+++ src/main/java/be/parkalot/knight_parkalot/service/inputvalidation/DivisionInputValidation.java
@@ -2,40 +2,24 @@ package be.parkalot.knight_parkalot.service.inputvalidation;
 
 import be.parkalot.knight_parkalot.dto.CreateDivisionDto;
 import be.parkalot.knight_parkalot.repository.DivisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DivisionInputValidation {
 
-    private CreateDivisionDto createDivisionDto;
-    private DivisionRepository divisionRepository;
+    public void validate(CreateDivisionDto createDivisionDto) {
 
-    public DivisionInputValidation(CreateDivisionDto createDivisionDto, DivisionRepository divisionRepository) {
-        this.createDivisionDto = createDivisionDto;
-        this.divisionRepository = divisionRepository;
-    }
-
-    public void validate(CreateDivisionDto createDivisionDto){
-        if(createDivisionDto.getParentId()!=0 && !divisionRepository.existsById(createDivisionDto.getParentId())){
-            throw new IllegalArgumentException("Division parent Id is not Existing");
-        }
-
-        if(createDivisionDto.getName() == null){
+        if (createDivisionDto.getName() == null) {
             throw new IllegalArgumentException("Please fill the name");
         }
 
-        if(createDivisionDto.getDirectorName()==null){
+        if (createDivisionDto.getDirectorName() == null) {
             throw new IllegalArgumentException("Please fill the Director name");
         }
 
-
-        if(createDivisionDto.getOldName() == null){
+        if (createDivisionDto.getOldName() == null) {
             throw new IllegalArgumentException("Please fill the Old division name");
         }
-
-
-
-
     }
-
-
-
 }
