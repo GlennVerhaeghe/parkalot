f7d67effff6ee1639f5fd85cac87e3d39084ccb5
Klaas Devits <klaas.devits@gmail.com>
2021-11-17 16:05:10 +0100

    Updated switchSecurity

--- src/main/java/be/parkalot/knight_parkalot/api/DivisionController.java
+++ src/main/java/be/parkalot/knight_parkalot/api/DivisionController.java
@@ -1,9 +1,9 @@
 package be.parkalot.knight_parkalot.api;
 
-import be.parkalot.knight_parkalot.domain.Division;
 import be.parkalot.knight_parkalot.dto.CreateDivisionDto;
 import be.parkalot.knight_parkalot.dto.DivisionDto;
 import be.parkalot.knight_parkalot.service.DivisionService;
+import be.parkalot.knight_parkalot.switchsecure.SecurityGuard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +32,7 @@ public class DivisionController {
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
+    @SecurityGuard(SecurityGuard.ApiUserRole.MANAGER)
     public DivisionDto createNewDivision(@RequestBody CreateDivisionDto divisionDto) {
         return service.createNewDivision(divisionDto);
     }
---
+++ src/main/java/be/parkalot/knight_parkalot/switchsecure/UserSecurityInformation.java
@@ -0,0 +1,22 @@
+package be.parkalot.knight_parkalot.switchsecure;
+
+public class UserSecurityInformation {
+    private String name;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
