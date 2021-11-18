ea2294d85c1ed63ea86b35f47e73c4e7f196c26d
Klaas Devits <klaas.devits@gmail.com>
2021-11-18 10:11:45 +0100

    deleted unnecessary imports

--- src/main/java/be/parkalot/knight_parkalot/repository/MemberRepository.java
+++ src/main/java/be/parkalot/knight_parkalot/repository/MemberRepository.java
@@ -3,10 +3,7 @@ package be.parkalot.knight_parkalot.repository;
 import be.parkalot.knight_parkalot.domain.LicensePlate;
 import be.parkalot.knight_parkalot.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
     Member findByEmail(String email);
