c5b68ddac5e4ddd53afd0a48cd9f1da47ec2312f
Klaas Devits <klaas.devits@gmail.com>
2021-11-19 11:51:29 +0100

    added entities for invoice and invoiceItem

---
+++ src/main/java/be/parkalot/knight_parkalot/domain/InvoiceItem.java
@@ -0,0 +1,39 @@
+package be.parkalot.knight_parkalot.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "invoice_item")
+public class InvoiceItem {
+
+    @Id
+    @SequenceGenerator(name = "invoice_item_seq", sequenceName = "INVOICE_ITEM_ID_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "invoice_item_seq")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "parking_spot_allocation_id", referencedColumnName = "id")
+    private ParkingSpotAllocation parkingSpotAllocation;
+
+    @Column(name = "price")
+    private double price;
+
+    public InvoiceItem() {
+    }
+
+    public ParkingSpotAllocation getParkingSpotAllocation() {
+        return parkingSpotAllocation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
